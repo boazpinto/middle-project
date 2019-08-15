53fe9194bc42917b71d08b772737965b65fa90a6
boazpinto <pintob@gmail.com>
2019-08-15 13:24:05 +0300

    finish main html

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 var myVar;
-var i=0;
+var i = 0;
 
 function myFunction() {
-  myVar = setInterval(changePic, 3000);
+
+    myVar = setInterval(changePic, 3000);
 }
 
 function changePic() {
-    if (i=0) {
-        i=1;
-document.getElementById("change").src="/mages/images/1.jpg";
+    
+    if (i == 0) {
+        i = 1;
+        document.getElementById("change").src = "/images/images/1.jpg";
 
-      } else if (i=1) {
-          i=2
-          document.getElementById("change").src="/mages/images/2.jpg";
-    } else if (i=1) {
-        i=3;
-        document.getElementById("change").src="/mages/images/3.jpg"; 
+    } else if (i == 1) {
+        i = 2
+        document.getElementById("change").src = "/images/images/2.jpg";
+    } else if (i == 2) {
+        i = 3;
+        document.getElementById("change").src = "/images/images/3.jpg";
     } else {
-        i=4;
-        document.getElementById("change").src="/mages/images/4.jpg";
-      }
+        i = 0;
+        document.getElementById("change").src = "/images/images/4.jpg";
+    }
 }
