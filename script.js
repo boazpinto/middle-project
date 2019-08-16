f3f40006a9a2a8a994433b8bd90be87fd0d694ae
boazpinto <pintob@gmail.com>
2019-08-16 16:12:20 +0300

    fixing not showing pictures problem

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ function changePic() {
     
     if (i == 0) {
         i = 1;
-        document.getElementById("change").src = "/images/images/1.jpg";
+        document.getElementById("change").src = "images/images/1.jpg";
 
     } else if (i == 1) {
         i = 2
-        document.getElementById("change").src = "/images/images/2.jpg";
+        document.getElementById("change").src = "images/images/2.jpg";
     } else if (i == 2) {
         i = 3;
-        document.getElementById("change").src = "/images/images/3.jpg";
+        document.getElementById("change").src = "images/images/3.jpg";
     } else {
         i = 0;
-        document.getElementById("change").src = "/images/images/4.jpg";
+        document.getElementById("change").src = "images/images/4.jpg";
     }
 }
