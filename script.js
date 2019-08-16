ea8fd1edf25c3aba9b618f3c881ce4dda7121837
boazpinto <pintob@gmail.com>
2019-08-16 17:42:50 +0300

    changing the js using arrays

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 //   every 3 seconds replacing the picture in the home page
+
 var myVar;
 var i = 0;
+var file=["images/images/1.jpg","images/images/2.jpg",
+            "images/images/3.jpg","images/images/4.jpg"]
+
 
 //   this function setting an interval of 3 second and call the function
 //   changePic at the end of each interval
+
 function myFunction() {
 
     myVar = setInterval(changePic, 3000);
 }
 //    this function replaces the picture in home page in an order
-function changePic() {
-    
-    if (i == 0) {
-        i = 1;
-        document.getElementById("change").src = "images/images/1.jpg";
 
-    } else if (i == 1) {
-        i = 2
-        document.getElementById("change").src = "images/images/2.jpg";
-    } else if (i == 2) {
-        i = 3;
-        document.getElementById("change").src = "images/images/3.jpg";
-    } else {
-        i = 0;
-        document.getElementById("change").src = "images/images/4.jpg";
+function changePic() {
+    document.getElementById("change").src =file[i];
+    i++;
+    if (i==4){
+        i=0;
     }
 }
+
